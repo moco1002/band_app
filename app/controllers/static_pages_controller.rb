3a938649f21761dbcb27193efa719c7720ef4f01
moco1002 <moco1002web@gmail.com>
2020-03-23 17:38:42 +0900

    add a StaticPages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def top
+  end
+
+  def help
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/top'
+  get 'static_pages/help'
+  get 'static_pages/about'
 root 'application#hello'
 end
