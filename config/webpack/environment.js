027e650ae167de7832e02898a01eebf11ed6b8a2
moco1002 <moco1002web@gmail.com>
2020-03-25 11:48:34 +0900

    re

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -15,4 +15,30 @@ environment.plugins.prepend(
     Popper: 'popper.js'
   })
 )
+
+module.exports = {
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin()
+  ]
+}
+
+module.exports = environment
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+module.exports = environment
+
+const customConfig = require('./custom')
+environment.config.merge(customConfig)
+
 module.exports = environment
