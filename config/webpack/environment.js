f9b5864a4e312f5aaa63d6b0ca5e9e1d596aebfb
moco1002 <moco1002web@gmail.com>
2020-03-25 16:40:23 +0900

    js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,19 +2,7 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-environment.config.resolve.alias = {'vue$': 'vue/dist/vue.esm.js' }
-// jQueryとBootstapのJSを使えるように
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: 'popper.js'
-  })
-)
+
 
 module.exports = {
   module: {
@@ -33,6 +21,7 @@ module.exports = {
 }
 
 module.exports = environment
+
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
